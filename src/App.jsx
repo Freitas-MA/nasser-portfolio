b2ae25b7c50fef1dc650b11f67140f873c749d4f
Freitas-MA <freitas.ma.1993@gmail.com>
2023-12-14 22:31:04 +0000

    Add Vercel Analytics and Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import { Navbar } from "./components";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "./theme/Theme";
 import Footer from "./components/Footer/Footer";
+import Cookies from "./components/Cookies/Cookies";
 // Router component
 import { Outlet } from "react-router-dom";
+// Vercel Analytics and Speed Insights
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -13,7 +17,10 @@ function App() {
         <GlobalStyle />
         <Navbar />
         <Outlet />
+        <Cookies />
         <Footer />
+        <SpeedInsights />
+        <Analytics />
       </ThemeProvider>
     </>
   );
